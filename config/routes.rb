37a53055e3d810b29d62340d872bb115f8d1c594
David Peña <david.pena@youse.com.co>
2019-04-06 12:53:25 -0500

    Front update

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :raffles
   resources :parkings
   resources :applicants
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+
+  get 'home/index', to: 'home#index'
+
 end
